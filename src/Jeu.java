aab7a82aec7caf1b8c3820f17bc11a3154324e2a
ImFonky <pbmonaco@outlook.com>
2017-02-20 11:00:26 +0100

    Modif PB

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,10 +1,13 @@
 /**
  * Created by Fonky on 20.02.2017.
  */
+
 public class Jeu
 {
    public static void main(String... args)
    {
+      Espace monUnivers = new Espace();
 
+      monUnivers.addNewPlanet(5.9736E24,6371.008);
    }
 }
