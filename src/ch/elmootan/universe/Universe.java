c0aec33f71662fa8429097ab5fffeb5168ea59f1
Lucas Elisei <faku99dev@gmail.com>
2017-03-16 18:59:41 +0100

    Working version. TODO: MVC model

--- src/ch/elmootan/universe/Universe.java
+++ src/ch/elmootan/universe/Universe.java
@@ -8,9 +8,9 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import ch.elmootan.physics.*;
-import ch.elmootan.ui.Refresher;
 
 public class Universe extends Frame {
 
@@ -50,17 +50,86 @@ public class Universe extends Frame {
 
         Timer myTime = new Timer("myTime");
 
-        myTime.schedule(new Refresher(), 0, 33);
+        myTime.schedule(new TimerTask() {
 
+            @Override
+            public void run() {
+                for (int i = 0; i < allThings.size(); ++i) {
+                    Body body = allThings.get(i);
+                    for (int j = i + 1; j < allThings.size(); ++j) {
+                        Body surrounding = allThings.get(j);
+
+                        //On calcule les distances x et y et la distance au carré
+                        double dX = surrounding.getPosition().getX() - body.getPosition().getX();
+                        double dY = surrounding.getPosition().getY() - body.getPosition().getY();
+                        double sqDistance = dX * dX + dY * dY;
+
+                        //On calcule la distance réelle (Ground to ground)
+                        double gTgDistance = Math.sqrt(sqDistance) - body.getRadius() / 2 - surrounding.getRadius() / 2;
+
+                        if (gTgDistance < 0) //Collision!
+                        {
+                            if (body.getMass() > surrounding.getMass()) {
+                                body.eat(surrounding);
+                                allThings.remove(surrounding);
+                            } else {
+                                surrounding.eat(body);
+                                allThings.remove(body);
+                            }
+                        } else //Gravitation et physique
+                        {
+                            //On calcule le ratio des composantes de distance x et y (règle de 3, Thalès)
+                            double rDX = dX / Math.sqrt(sqDistance);
+                            double rDY = dY / Math.sqrt(sqDistance);
+
+                            //Loi de gravité : F [N] = G [N*m2*kg-2] * mA [kg] * mB [kg] / d2 [m2]
+                            //un Newton = 1 [kg*m*s-2]. Plus simplement [kg*a] ou a est l'accélération
+                            //donc G [kg*m*s-2*m2*kg-2] ==> G [m3*s-2*kg-1]
+                            //On peut donc calculer simplement l'accélération aN sur chaque corps avec :
+                            //aA [m*s-2] = G [m3*s-2*kg-1] * mB [kg] / d2 [m2]
+                            double bodyA = Constants.GRAVITATION.valeur * surrounding.getMass() / sqDistance;
+                            //A cette étape nous avons un vecteur d'accélération mais pas de direction
+                            //Il décomposer en deux composantes x et y
+                            double bodyAX = bodyA * rDX;
+                            double bodyAY = bodyA * rDY;
+
+                            //Même chose pour les deuxième corps
+                            double surrA = Constants.GRAVITATION.valeur * body.getMass() / sqDistance;
+                            double surrAX = surrA * -rDX;
+                            double surrAY = surrA * -rDY;
+
+                            //Il faut maintenant appliquer accélérations x et y aux vitesses x et y
+                            //Pour le moment la cadence du processeur règle la vitesse du programme
+                            body.getSpeed().setX(body.getSpeed().getX() + bodyAX);
+                            body.getSpeed().setY(body.getSpeed().getY() + bodyAY);
+
+                            surrounding.getSpeed().setX(surrounding.getSpeed().getX() + surrAX);
+                            surrounding.getSpeed().setY(surrounding.getSpeed().getY() + surrAY);
+
+                        }
+                    }
+
+                    body.getPosition().setX(body.getPosition().getX() + body.getSpeed().getX());
+                    body.getPosition().setY(body.getPosition().getY() + body.getSpeed().getY());
+
+                    // Freinage des corps
+                    // body.speed.x -= 0.005 * body.speed.x;
+                    // body.speed.y -= 0.005 * body.speed.y;
+                }
+
+                res();
+            }
+
+        }, 0, 33);
     }
 
     public void paint(Graphics g) {
 
         for (Body body : allThings) {
-            int radius = (int) (body.radius / zoom);
-            int x = (getWidth() / 2) + ((int) ((body.position.x - (body.radius / 2)) / zoom));
-            int y = (getHeight() / 2) + ((int) ((body.position.y - (body.radius / 2)) / zoom));
-            g.setColor(body.couleur);
+            int radius = (int) (body.getRadius() / zoom);
+            int x = (getWidth() / 2) + ((int) ((body.getPosition().getX() - (body.getRadius() / 2)) / zoom));
+            int y = (getHeight() / 2) + ((int) ((body.getPosition().getY() - (body.getRadius() / 2)) / zoom));
+            g.setColor(body.getCouleur());
             g.drawOval(x, y, radius, radius);
         }
     }
