80176cca852118ee67dc171189792bd24eed0d1a
Lucas Elisei <lucas.elisei@heig-vd.ch>
2017-05-04 15:48:27 +0200

    Server can create games.

--- server/src/main/java/ch/elmootan/protocol/Protocol.java
+++ server/src/main/java/ch/elmootan/protocol/Protocol.java
@@ -4,7 +4,11 @@ public final class Protocol {
 
     public static final int PORT = 56787;
 
+    public static final String CMD_CREATE_GAME = "PLANET_IO_CREATE_GAME";
+
     public static final String CMD_DISCONNECT = "PLANET_IO_DISCONNECT";
+
+    public static final String CMD_FAILURE = "PLANET_IO_FAILURE";
 
     public static final String CMD_HELLO = "PLANET_IO_HELLO";
 
--- server/src/main/java/ch/elmootan/server/ClientHandler.java
+++ server/src/main/java/ch/elmootan/server/ClientHandler.java
@@ -1,16 +1,25 @@
 package ch.elmootan.server;
 
 import ch.elmootan.protocol.Protocol;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 
 import java.io.*;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ClientHandler {
 
-    final static Logger LOG = Logger.getLogger(ClientHandler.class.getName());
+    private final static Logger LOG = Logger.getLogger(ClientHandler.class.getName());
+
+    private final GamesManager gamesManager = GamesManager.getSharedManager();
+
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public ClientHandler() {
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     }
 
     public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
@@ -21,17 +30,38 @@ public class ClientHandler {
         boolean done = false;
         while (!done && ((command = reader.readLine()) != null)) {
             LOG.log(Level.INFO, "COMMAND: {0}", command);
-            switch (command.toUpperCase()) {
+            int index = command.toUpperCase().indexOf(':');
+            index = index != -1 ? index : command.length();
+            switch (command.toUpperCase().substring(0,  index)) {
+
+                // Client wants to create a game.
+                case Protocol.CMD_CREATE_GAME: {
+                    if (index != command.length()) {
+                        String data = command.substring(index + 1);
+                        gamesManager.addGame(data);
+                        writer.println(Protocol.CMD_SUCCESS);
+                    } else {
+                        writer.println(Protocol.CMD_FAILURE);
+                    }
+
+                    break;
+                }
 
                 // Client wants to disconnect.
                 case Protocol.CMD_DISCONNECT: {
                     done = true;
                     writer.println(Protocol.CMD_SUCCESS);
+
+                    break;
                 }
 
                 // Client wants to connect.
                 case Protocol.CMD_HELLO: {
-                    writer.println(Protocol.CMD_SUCCESS);
+                    String serializedData = mapper.writeValueAsString(gamesManager.getGamesList());
+
+                    writer.println(Protocol.CMD_SUCCESS + ":" + serializedData);
+
+                    break;
                 }
             }
             writer.flush();
