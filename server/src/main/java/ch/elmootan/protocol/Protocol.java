766edbacbea14bc667f89cee6164412a3a4f62d2
Lucas Elisei <lucas.elisei@heig-vd.ch>
2017-05-04 13:55:01 +0200

    Added Protocol

---
+++ server/src/main/java/ch/elmootan/protocol/Protocol.java
@@ -0,0 +1,11 @@
+package ch.elmootan.protocol;
+
+public final class Protocol {
+
+    public static final int PORT = 56787;
+
+    public static final String CMD_HELLO = "PLANET_IO_HELLO";
+
+    public static final String CMD_SUCCESS = "PLANET_IO_SUCCESS";
+
+}
